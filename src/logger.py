c9b9bd9afcdd00875ced075de194893473153393
zahidshaikh10101 <zahidshaikh10101@gmail.com>
2024-02-12 17:48:02 +0530

    Logger file

---
+++ src/logger.py
@@ -0,0 +1,21 @@
+import logging
+import os
+import sys
+from datetime import datetime
+
+# Logs collection file name 
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# Logs collection file path 
+logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename = LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+if __name__ == '__main__':
+    logging.info("Logging is working")
